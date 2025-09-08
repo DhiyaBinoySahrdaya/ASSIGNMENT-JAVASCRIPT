781921e509c3fc733806fbe4e9e92dddb26b4ee8
DhiyaBinoySahrdaya <dhiya224730@sahrdaya.ac.in>
2025-09-08 11:53:42 +0530

    Add files via upload

---
+++ sum.js
@@ -0,0 +1,16 @@
+function sumArray(numbers) {
+  let total = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i];
+  }
+  return total;
+}
+const myNumbers = [1, 2, 3, 4, 5];
+const result = sumArray(myNumbers);
+console.log(result); 
+
+const anotherArray = [10, 20, 30];
+console.log(sumArray(anotherArray)); 
+
+const emptyArray = [10,20,30,40,50];
+console.log(sumArray(emptyArray)); 
